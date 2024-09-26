ce8b049b568d389cc4736e0d8e544b8efa433843
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-09-26 23:01:41 +0300

    Add files via upload

---
+++ Scripts/SoundControlPanel.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundControlPanel : MonoBehaviour
+{
+    public const string MasterVolume = nameof(MasterVolume);
+    public const string SoundVolume = nameof(SoundVolume);
+    public const string MusicVolume = nameof(MusicVolume);
+
+    [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private AudioSource _audioSource;
+
+    private float _volumeBackgroundMusic;
+
+    public void TurnOn(bool enabled)
+    {
+        if (enabled)
+        {
+            ChangeVolume(MasterVolume, _audioSource.volume);
+        }
+    }
+
+    public void TurnOff(bool enabled)
+    {
+        if (enabled)
+        {
+            int volumeOff = -80;
+
+            ChangeVolume(MasterVolume, volumeOff);
+        }
+    }
+
+    public void ChangeMasterVolume(float volume)
+    {
+        _audioSource.volume = volume;
+
+        ChangeVolume(MasterVolume, _audioSource.volume);
+    }
+
+    public void ChangeVolumeButtons(float volume)
+    {
+        ChangeVolume(SoundVolume, volume);
+    }
+
+    public void ChangeVolumeBackgroundMusic(float volume)
+    {
+        _volumeBackgroundMusic = volume;
+
+        ChangeVolume(MusicVolume, _volumeBackgroundMusic);
+    }
+
+    private void ChangeVolume(string nameMixer, float volume)
+    {
+        _mixer.audioMixer.SetFloat(nameMixer, Mathf.Log10(volume) * 20);
+    }
+}
